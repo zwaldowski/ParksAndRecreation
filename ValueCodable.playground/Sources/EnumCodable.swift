91d324835be106c05904ae7e3696d26e9cff4d6f
Zachary Waldowski <zach@waldowski.me>
2016-03-10 12:54:20 -0500

    [Value coding] Remove extra unnecessary enum impl.

--- ValueCodable.playground/Sources/EnumCodable.swift
+++ ValueCodable.playground/Sources/EnumCodable.swift
@@ -12,21 +12,7 @@ extension RawRepresentable where RawValue: ValueCodable {
         guard let raw = coder.decodeValue(ofType: RawValue.self) else { return nil }
         self.init(rawValue: raw)
     }
-    
-}
 
-extension RawRepresentable where RawValue: SignedIntegerType {
-
-    /// Encodes `self` using a given archiver.
-    public func encode(with coder: NSCoder) {
-        coder.encodeInt64(numericCast(rawValue), forKey: NSKeyedArchiveRootObjectKey)
-    }
-
-    /// Creates an instance from from data in a given unarchiver.
-    public init?(coder: NSCoder) {
-        self.init(rawValue: numericCast(coder.decodeInt64ForKey(NSKeyedArchiveRootObjectKey)))
-    }
-    
 }
 
 extension RawRepresentable where RawValue: _ObjectiveCBridgeable, RawValue._ObjectiveCType: NSCoding, RawValue._ObjectiveCType: NSObject {
