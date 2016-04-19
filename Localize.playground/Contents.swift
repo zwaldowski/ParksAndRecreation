700e461dd1671392fcbadf4e01590e495c18e7e7
Zachary Waldowski <zach@waldowski.me>
2016-04-18 21:09:50 -0400

    [Localization] Better support for CVarArgType in formatting

--- Localize.playground/Contents.swift
+++ Localize.playground/Contents.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 let s1 = NSLocalizedString("test-string", comment: "Description used for the test")
-let s2 = localize("test-string", comment: "Description used for the test")
-let s3 = localize("test-progress-\(4)-of-\(5)", comment: "Help text used for a positional description")
+let s2 = localized("test-string", comment: "Description used for the test")
+let s3 = localized("test-progress-\(4)-of-\(5)", comment: "Help text used for a positional description")
+
+let s4 = localized("Your \(Int(0).formatted("%zd")) Friends", comment: "Numerical test describing your friends")
+let s5 = localized("Your \(Int(1).formatted("%zd")) Friends", comment: "Numerical test describing your friends")
+let s6 = localized("Your \(Int(2).formatted("%zd")) Friends", comment: "Numerical test describing your friends")
--- Localize.playground/Sources/LocalizableText.swift
+++ Localize.playground/Sources/LocalizableText.swift
@@ -1,54 +1,146 @@
 import Foundation
 
-public enum LocalizableText {
-    case Tree([LocalizableText])
-    case Segment(String)
-    case Expression(String)
+// Extra inspiration from https://gist.github.com/brentdax/79fa038c0af0cafb52dd
+
+public struct LocalizableText {
+    private enum Storage {
+        case Tree([LocalizableText])
+        case Expression(() -> String)
+        case Segment(String)
+        case Formatted(CVarArgType, StaticString)
+        case Literal(StaticString)
+    }
+
+    private let storage: Storage
+
+    private init(_ storage: Storage) {
+        self.storage = storage
+    }
 }
 
-extension LocalizableText: CustomStringConvertible {
-    
-    public var description: String {
-        switch self {
-        case .Tree(let segments):
-            return segments.reduce("") { $0 + String($1) }
+extension LocalizableText: CustomDebugStringConvertible {
+    public var debugDescription: String {
+        switch storage {
+        case .Tree(let elements):
+            return String(elements)
+        case .Literal(let literal):
+            return String(reflecting: literal)
         case .Segment(let segment):
-            return String(segment)
-        case .Expression(let value):
-            return String(value)
+            return String(reflecting: segment)
+        case .Formatted(let value, let format):
+            return "(\(value.dynamicType) via \"\(format)\")"
+        case .Expression:
+            return "(unresolved)"
         }
     }
-    
 }
 
 extension LocalizableText: StringInterpolationConvertible {
-    
     public init(stringInterpolation elements: LocalizableText...) {
-        self = .Tree(elements)
+        self.storage = .Tree(elements)
     }
-    
+
     public init<T>(stringInterpolationSegment expression: T) {
-        if let expression = expression as? String {
-            self = .Segment(expression)
-        } else {
-            self = .Expression(String(expression))
-        }
+        self.storage = .Expression({
+            String(expression)
+        })
     }
-    
+
+    public init(stringInterpolationSegment expression: String) {
+        self.storage = .Segment(expression)
+    }
+
+    public init<T: NSObject>(stringInterpolationSegment expression: T) {
+        self.storage = .Formatted(expression, "%@")
+    }
+
+    public init(stringInterpolationSegment expression: LocalizableText) {
+        self = expression
+    }
 }
 
 extension LocalizableText: StringLiteralConvertible {
-    
-    public init(stringLiteral value: String) {
-        self = .Segment(value)
+    public init(stringInterpolationSegment expression: StaticString) {
+        self.storage = .Literal(expression)
     }
-    
-    public init(extendedGraphemeClusterLiteral value: Character) {
-        self = .Segment(String(value))
+
+    public init(stringLiteral value: StaticString) {
+        self.init(stringInterpolationSegment: value)
     }
-    
-    public init(unicodeScalarLiteral value: UnicodeScalar) {
-        self = .Segment(String(value))
+
+    public init(extendedGraphemeClusterLiteral value: StaticString) {
+        self.init(stringInterpolationSegment: value)
     }
+
+    public init(unicodeScalarLiteral value: StaticString) {
+        self.init(stringInterpolationSegment: value)
+    }
+}
+
+extension CVarArgType {
+
+    public func formatted(format: StaticString) -> LocalizableText {
+        return LocalizableText(.Formatted(self, format))
+    }
+
+}
+
+private extension LocalizableText {
+
+    func withFormatSegments(@noescape eachSegment: String -> Void) {
+        switch storage {
+        case .Tree(let tree):
+            for item in tree {
+                item.withFormatSegments(eachSegment)
+            }
+        case .Expression:
+            eachSegment("%@")
+        case .Segment(let segment):
+            eachSegment(segment)
+        case .Formatted(_, let format):
+            eachSegment(format.stringValue)
+        case .Literal(let literal):
+            eachSegment(literal.stringValue)
+        }
+    }
+
+    var argumentCount: Int {
+        switch storage {
+        case .Tree(let tree):
+            return tree.reduce(0) { $0 + $1.argumentCount }
+        case .Expression, .Formatted:
+            return 1
+        case .Segment, .Literal:
+            return 0
+        }
+    }
+
+    func withArguments(@noescape eachArgument: CVarArgType -> Void) {
+        switch storage {
+        case .Tree(let tree):
+            for item in tree {
+                item.withArguments(eachArgument)
+            }
+        case .Expression(let getter):
+            eachArgument(getter())
+        case .Formatted(let value, _):
+            eachArgument(value)
+        case .Segment, .Literal:
+            break
+        }
+    }
+
+}
+
+public func localized(text: LocalizableText, withTable tableName: String? = nil, inBundle bundle: NSBundle = NSBundle.mainBundle(), @autoclosure comment _: () -> String) -> String {
+    var format = ""
+    text.withFormatSegments { format += $0 }
+
+    let localizedFormat = bundle.localizedStringForKey(format, value: format, table: tableName)
+
+    var arguments = [CVarArgType]()
+    arguments.reserveCapacity(text.argumentCount)
+    text.withArguments { arguments.append($0) }
     
+    return String(format: localizedFormat, arguments: arguments)
 }
