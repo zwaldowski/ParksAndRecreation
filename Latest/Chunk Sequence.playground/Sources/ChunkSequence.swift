4877a4d33d746e189ae6e79e3b6d70c2284f7651
Zachary Waldowski <zach@waldowski.me>
2019-11-03 13:59:33 -0500

    ChunkSequence: Remove unneeded end cache

--- Latest/Chunk Sequence.playground/Sources/ChunkSequence.swift
+++ Latest/Chunk Sequence.playground/Sources/ChunkSequence.swift
@@ -4,19 +4,17 @@ public struct ChunkSequence<Base: Collection>: Sequence, IteratorProtocol {
 
     private let base: Base
     private let stride: Int
-    private let limit: Base.Index
     private var start: Base.Index
 
     fileprivate init(stride: Int, in base: Base) {
         precondition(stride > 0)
         self.base = base
         self.stride = stride
-        self.limit = base.endIndex
         self.start = base.startIndex
     }
 
     public mutating func next() -> Base.SubSequence? {
-        guard start != limit else { return nil }
+        guard start != base.endIndex else { return nil }
         let slice = base.suffix(from: start).prefix(stride)
         start = slice.endIndex
         return slice
