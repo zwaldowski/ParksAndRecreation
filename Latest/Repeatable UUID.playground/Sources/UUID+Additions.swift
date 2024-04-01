8758bcfeadb676375c313a22728483b6cc28aa26
Zachary Waldowski <zach@waldowski.me>
2024-03-31 23:59:06 -0700

    Update UUID to CryptoKit

--- Latest/Repeatable UUID.playground/Sources/UUID+Additions.swift
+++ Latest/Repeatable UUID.playground/Sources/UUID+Additions.swift
@@ -1,5 +1,5 @@
 import Foundation
-import CommonCrypto
+import CryptoKit
 
 extension UUID {
 
@@ -12,28 +12,26 @@ extension UUID {
     /// string combined from many sources) without having to pay the cost of
     /// storing and comparing that input data.
     public init(hashing value: String, inNamespace namespace: UUID) {
-        var context = CC_SHA1_CTX()
-        CC_SHA1_Init(&context)
+        var context = Insecure.SHA1()
 
-        _ = withUnsafeBytes(of: namespace.uuid) { (buffer) in
-            CC_SHA1_Update(&context, buffer.baseAddress, CC_LONG(buffer.count))
+        withUnsafeBytes(of: namespace.uuid) { bufferPointer in
+            context.update(bufferPointer: bufferPointer)
         }
 
-        _ = value.withCString { (cString) in
-            CC_SHA1_Update(&context, cString, CC_LONG(strlen(cString)))
-        }
-
-        var array = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
-        CC_SHA1_Final(&array, &context)
-
-        array[6] = (array[6] & 0x0F) | 0x50 // set version number nibble to 5
-        array[8] = (array[8] & 0x3F) | 0x80 // reset clock nibbles
+        context.update(data: Data(value.utf8))
 
         // truncate to first 16
-        self.init(uuid: (array[0], array[1], array[2], array[3],
-                         array[4], array[5], array[6], array[7],
-                         array[8], array[9], array[10], array[11],
-                         array[12], array[13], array[14], array[15]))
+        var uuid = context.finalize().withUnsafeBytes {
+            ($0[0], $0[1], $0[2], $0[3],
+             $0[4], $0[5], $0[6], $0[7],
+             $0[8], $0[9], $0[10], $0[11],
+             $0[12], $0[13], $0[14], $0[15])
+        }
+
+        uuid.6 = (uuid.6 & 0x0F) | 0x50 // set version number nibble to 5
+        uuid.8 = (uuid.8 & 0x3F) | 0x80 // reset clock nibbles
+
+        self.init(uuid: uuid)
     }
 
 }
